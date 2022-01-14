83a1bafe09cc672e829b19021f3fc7cacce65e15
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-14 23:53:28 +0100

    Create recc.lua

---
+++ recc.lua
@@ -0,0 +1,10 @@
+pcall(function()
+		while wait(1) do
+			for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+				if v:FindFirstChild("Setting") then
+					local a =  require(v.Setting)
+					a.CameraShakingEnabled = false
+				end
+			end
+		end
+  end)
