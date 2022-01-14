94f54787dedffcca6db3f9bf55fb1d366c019a3a
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-14 23:20:37 +0100

    Update clip.lua

--- clip.lua
+++ clip.lua
@@ -1,19 +1,9 @@
-pcall(function()
-noclip = false
-	game:GetService('RunService').Stepped:connect(function()
-		if noclip then
-			game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-		end
-	end)
-	plr = game.Players.LocalPlayer
-	mouse = plr:GetMouse()
-	mouse.KeyDown:connect(function(key)
-
-		if key == "x" then
-			noclip = not noclip
-			game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-		end
-	end)
-	print('Loaded')
-	print('Press "X" to noclip')
+game:GetService("RunService").Stepped:Connect(function()
+   if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.X) then
+       for _, v in pairs(game:GetService("Players").LocalPlayer.Character:GetChildren()) do
+           if v:IsA("BasePart") then
+               v.CanCollide = false
+           end
+       end
+   end
 end)
