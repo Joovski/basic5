6f132a9ea2f8708c7ec385f51d4b0f10f7d5cc35
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-15 00:45:41 +0100

    Create jail.lua

---
+++ jail.lua
@@ -0,0 +1,3 @@
+local player = game.Players.LocalPlayer
+local character = player.Character
+character:WaitForChild("HumanoidRootPart").CFrame = game:GetService("Workspace").INTERACTIONS.PrisonEscape.EscapedTrigger.CFrame
