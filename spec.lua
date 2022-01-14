fe3dbf4090f1e953a76b6f657ad338e61af7a345
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-15 00:38:20 +0100

    Delete spec.lua

--- spec.lua
+++
@@ -1,9 +0,0 @@
-	if txt then
-		local text = (txt)
-		local players = game.Players:GetChildren()
-		for _,v in pairs (players) do
-			if string.match(string.lower(v.Name),string.lower(text)) then
-				workspace.Camera.CameraSubject = v.Character
-			end
-		end
-	end
